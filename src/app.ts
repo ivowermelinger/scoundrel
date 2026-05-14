5f86caa3a2cc2595bb2552760e4bb3715bfcc542
Ivo Wermelinger <ivo_wermelinger@hotmail.com>
2026-05-14 11:17:29 +0200

    Style setup done

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import engine from './components/engine';
 
 import './styles/reset.css';
 import './styles/app.css';
+import './styles/engine.css';
+import './styles/card.css';
 
 Alpine.data('engine', engine);
 Alpine.start();
--- src/types/GameState.ts
+++ src/types/GameState.ts
@@ -3,5 +3,6 @@ import type { Card } from "./Card";
 export type GameState = {
     health: number;
     score: number;
+    cardsInRoom: Card[];
     selectedWeapon: Card | null;
 };
